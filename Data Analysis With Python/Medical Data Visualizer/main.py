969fddbc6b0ef50b91b564516c99b2a7494c6fbf
arshia-norouzi <arshia.norouzi2002@gmail.com>
2025-11-15 11:40:51 +0330

    Update main.py

--- Data Analysis With Python/Medical Data Visualizer/main.py
+++ Data Analysis With Python/Medical Data Visualizer/main.py
@@ -1,1 +1,14 @@
+# run_medical_plots.py
 
+import matplotlib.pyplot as plt
+import medical_data_visualizer as viz
+
+if __name__ == "__main__":
+    # Generate the categorical plot
+    cat_plot_figure = viz.draw_cat_plot()
+
+    # Generate the correlation heatmap
+    heatmap_figure = viz.draw_heat_map()
+
+    # Display both figures
+    plt.show()
