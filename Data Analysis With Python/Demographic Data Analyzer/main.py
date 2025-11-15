68530679f73aca7c0f5393204ef1bc8a36de986f
arshia-norouzi <arshia.norouzi2002@gmail.com>
2025-11-15 11:35:38 +0330

    Update main.py

--- Data Analysis With Python/Demographic Data Analyzer/main.py
+++ Data Analysis With Python/Demographic Data Analyzer/main.py
@@ -1,1 +1,12 @@
+# run_analysis.py
+import pandas as pd
+from demographic_data_analyzer import calculate_demographic_data as analyze_data
 
+# Load dataset
+dataset = pd.read_csv("adult.data.csv")
+
+# Perform analysis
+analysis_output = analyze_data(dataset)
+
+# Display results
+print(analysis_output)
